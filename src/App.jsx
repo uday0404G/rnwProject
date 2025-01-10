ca8f9b711b78d04e92bda70ae079abfdc220bb2d
Uday Lashkari <102202156+uday0404G@users.noreply.github.com>
2025-01-10 19:28:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MainRoute from './Routes/mainRoute'
+import MainRoute from './Routes/MainRoute'
 
 function App() {
   const [count, setCount] = useState(0)
